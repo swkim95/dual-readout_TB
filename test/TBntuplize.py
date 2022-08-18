a161bfd184b5e78e4f1788aaa8ad5c557c8387b4
Sanghyun Ko <sang.hyun.ko@cern.ch>
2022-08-19 08:21:57 +0900

    add argparser for TBntuplize

--- test/TBntuplize.py
+++ test/TBntuplize.py
@@ -1,22 +1,40 @@
 import pydrcTB
+import sys, os
+import argparse
+import ROOT
 
-filenames = [
-    "cal_fast_1_117.dat",
-    "cal_fast_2_117.dat",
-    "cal_fast_3_117.dat",
-    "cal_fast_4_117.dat",
-    "cal_fast_5_117.dat",
-    "cal_fast_6_117.dat",
-    "cal_fast_7_117.dat",
-    "cal_fast_8_117.dat",
-    "cal_fast_9_117.dat",
-    "cal_fast_10_117.dat",
-    "cal_fast_11_117.dat",
-    "cal_fast_12_117.dat",
-    "cal_fast_13_117.dat",
-    "cal_fast_14_117.dat",
-    "cal_fast_15_117.dat"
-]
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--RunNumber', '-rn', action='store', type=str, required=True, help='Run number')
+parser.add_argument('--FileNumber', '-fn', action='store', type=str, required=True, help='File nubver on run')
+
+args = parser.parse_args()
+
+rn=args.RunNumber
+fn=args.FileNumber
+
+WaveFiles=[]
+FastFiles=[]
+
+for mid in range(1,16):
+    WaveDat=f"/Users/drc_daq/scratch/Aug2022TB/SSD/SSD_Run_{rn}/Run_{rn}_Wave/Run_{rn}_Wave_MID_{mid}/Run_{rn}_Wave_MID_{mid}_FILE_{fn}.dat"
+    FastDat=f"/Users/drc_daq/scratch/Aug2022TB/SSD/SSD_Run_{rn}/Run_{rn}_Fast/Run_{rn}_Fast_MID_{mid}/Run_{rn}_Fast_MID_{mid}_FILE_{fn}.dat"
+
+    if(os.path.isfile):
+        WaveFiles.append(WaveDat)
+    else:
+        print(WaveDat,"not found")
+
+    if(os.path.isfile):
+        FastFiles.append(FastDat)
+    else:
+        print(FastDat,"not found")
+
+nameWave = f"ntuple_Run_{rn}_Wave_FILE_{fn}.root"
+nameFast = f"ntuple_Run_{rn}_Fast_FILE_{fn}.root"
 
 reader = pydrcTB.TBread()
-reader.ntuplizeFastmode(filenames,"test.root")
+reader.ntuplizeWaveform(WaveFiles,nameWave)
+
+reader = pydrcTB.TBread()
+reader.ntuplizeFastmode(FastFiles,nameFast)
