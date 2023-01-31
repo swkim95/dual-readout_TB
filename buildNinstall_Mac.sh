9d8decf63778f0a0604a6e4023bfdf8bc4863e5d
swkim95 <sungwon.kim@cern.ch>
2023-01-31 17:45:48 +0900

    removing redundant scripts for merging to KY's repo

--- buildNinstall_Mac.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/zsh
-
-source envset_Mac.sh
-mkdir build install
-cd build
-cmake -DCMAKE_INSTALL_PREFIX=../install -DPYTHON_LIBRARY=/opt/homebrew/Cellar/python@3.10/3.10.8/Frameworks/Python.framework/Versions/3.10/lib/libpython3.10.dylib -DPYTHON_INCLUDE_DIR=/opt/homebrew/Cellar/python@3.10/3.10.8/Frameworks/Python.framework/Versions/3.10/include/python3.10 ..
-make -j8 install
-cd ..
